1c21fdf171a56e43f0a899ca3d1ec6ac7cadc09d
qweu-studio <56268258+qweu-studio@users.noreply.github.com>
2021-07-09 20:20:51 +0300

    add build file

---
+++ build/Windows64/palette2paint.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'palette2paint.exe' with Python DLL being found.
+set PATH=c:\users\vadim\appdata\local\programs\python\python39;%PATH%
+"%~dp0.\palette2paint.exe"
+
